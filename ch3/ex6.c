4f08f4c58d01027aa53f6dfba1b19894e17b7f83
htomaka <honore.tomaka@gmail.com>
2019-06-11 22:26:03 +0200

    completed ch3 ex6

---
+++ ch3/ex6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int answer, result;
+
+    answer = 100;
+    result = answer - 10;
+    printf("The result is %d\n", result + 5); // 95
+    return 0;
+}
