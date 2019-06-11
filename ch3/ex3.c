94341914145fb8885a3f90a81df9c775ebf4238d
htomaka <honore.tomaka@gmail.com>
2019-06-11 22:11:43 +0200

    completed ch3 ex3

---
+++ ch3/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Testing...");
+    printf("....1");
+    printf("...2");
+    printf("..3");
+    printf("\n");
+}
+
+// Prints Testing.......1...2..3
