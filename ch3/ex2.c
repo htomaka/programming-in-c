c571115bbefd171fda1fbb3a928678c8286fc64a
htomaka <honore.tomaka@gmail.com>
2019-06-11 21:51:56 +0200

    completed ch3 ex 2

---
+++ ch3/ex2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("In C, lowercase letters are significant.\n");
+    printf("main is where program execution begins.\n");
+    printf("Open and closed braces enclose program statements in a routine.\n");
+    printf("All program statements must be terminated by a smeicolon.\n");
+    return 0;
+}
